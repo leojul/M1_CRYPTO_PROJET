521acea42f5cc47573053d549608c0af76d828a2
leojul <jules.diard@gmail.com>
2019-04-30 13:35:21 +0200

    ajout de ma partie

--- birthday_attack.h
+++ birthday_attack.h
@@ -2,7 +2,13 @@
 #define B_ATTACK
 
 //-----------------------------------------------
-//A COMPLETER
+int64_t attack(uint8_t *pt, uint8_t *ct, size_t ctlen);
+
+uint8_t *creation_message();
+
+int XOR(uint8_t *pt, uint8_t *ct, int x, int y );
+
+uint64_t calcul_Xor(uint8_t *ct, int i, int j);
 
 //-----------------------------------------------
 
